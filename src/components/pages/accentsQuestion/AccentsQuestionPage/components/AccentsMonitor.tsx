1f4d6a983094bb3921c6153ee62410b5aced1231
chihatw <chiha.tw@gmail.com>
2022-03-30 11:00:07 +0900

    update pitch-line

--- src/components/pages/accentsQuestion/AccentsQuestionPage/components/AccentsMonitor.tsx
+++ src/components/pages/accentsQuestion/AccentsQuestionPage/components/AccentsMonitor.tsx
@@ -1,71 +1,70 @@
-import Speaker from '@bit/chihatw.lang-gym.speaker';
-import { Box, Checkbox, Grid } from '@mui/material';
-import { Audio } from '../../../../../entities/Audio';
-import { buildAccents } from '../../../../../entities/Accent';
 import React from 'react';
+import Speaker from '@bit/chihatw.lang-gym.speaker';
 import VolumeOff from '@mui/icons-material/VolumeOff';
-import { PitchLine } from '@chihatw/lang-gym-h.ui.pitch-line';
+import { Checkbox } from '@mui/material';
+import { PitchLine } from '@chihatw/pitch-line.pitch-line';
+import string2PitchesArray from 'string2pitches-array';
+
+import { Audio } from '../../../../../entities/Audio';
 
 const AccentsMonitor: React.FC<{
-  accentString: string;
   audios: Audio[];
+  accentString: string;
   disabledsArray: number[][];
   onChangeDisabled: (sentenceIndex: number, wordIndex: number) => void;
 }> = ({ accentString, audios, disabledsArray, onChangeDisabled }) => {
+  const lines = accentString.split('\n');
   return (
-    <Box fontSize={12} color='#555'>
-      <Grid container direction='column' spacing={1}>
-        {accentString.split('\n').map((a, sentenceIndex) => {
+    <div style={{ fontSize: 12, color: '#555' }}>
+      <div style={{ display: 'grid', rowGap: 8 }}>
+        {lines.map((line, sentenceIndex) => {
           const audio = audios[sentenceIndex];
+          const pitchesArray = string2PitchesArray(line);
           return (
-            <Grid item key={sentenceIndex}>
-              <Grid container alignItems='center' spacing={2}>
-                <Grid item>{sentenceIndex}</Grid>
-                <Grid item>
-                  {!!audio.downloadURL ? (
-                    <Speaker
-                      start={audio.start}
-                      end={audio.end}
-                      downloadURL={audio.downloadURL}
-                    />
-                  ) : (
-                    <VolumeOff color='primary' />
-                  )}
-                </Grid>
-              </Grid>
-              <Grid container spacing={1}>
-                {buildAccents(a).map((wordAccent, wordIndex) => {
+            <div key={sentenceIndex}>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ paddingRight: 16 }}>{sentenceIndex}</div>
+                {!!audio.downloadURL ? (
+                  <Speaker
+                    start={audio.start}
+                    end={audio.end}
+                    downloadURL={audio.downloadURL}
+                  />
+                ) : (
+                  <VolumeOff color='primary' />
+                )}
+              </div>
+              <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {pitchesArray.map((pitches, wordIndex) => {
                   const isDisable =
                     disabledsArray[sentenceIndex].includes(wordIndex);
                   return (
-                    <Grid item key={wordIndex}>
-                      <Box
-                        p={1}
-                        borderRadius={4}
-                        bgcolor={isDisable ? '#eee' : 'transparent'}
-                      >
-                        <PitchLine
-                          moras={wordAccent.moras}
-                          pitchPoint={wordAccent.pitchPoint}
-                        />
-                        <Checkbox
-                          checked={isDisable}
-                          size='small'
-                          color='primary'
-                          onChange={() => {
-                            onChangeDisabled(sentenceIndex, wordIndex);
-                          }}
-                        />
-                      </Box>
-                    </Grid>
+                    <div
+                      key={wordIndex}
+                      style={{
+                        padding: '8px 16px',
+                        borderRadius: 4,
+                        backgroundColor: isDisable ? '#eee' : 'transparent',
+                      }}
+                    >
+                      <PitchLine pitches={pitches} />
+                      <Checkbox
+                        size='small'
+                        color='primary'
+                        checked={isDisable}
+                        onChange={() => {
+                          onChangeDisabled(sentenceIndex, wordIndex);
+                        }}
+                      />
+                    </div>
                   );
                 })}
-              </Grid>
-            </Grid>
+              </div>
+            </div>
           );
         })}
-      </Grid>
-    </Box>
+      </div>
+    </div>
   );
 };
 
